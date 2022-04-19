b730f95c777204f61a8bb736348b390d713410d5
Wagner Pedroso <95456109+wagnerpedroso@users.noreply.github.com>
2022-04-19 16:28:07 -0300

    Add files via upload
    Aula 1 Módulo 2 Semana 5 - Versão 1.0

---
+++ M2S5/exercicio/script.js
@@ -0,0 +1,23 @@
+//const soma = x => console.log(sobrenome)
+
+
+//localStorage.setItem('nome', 'Wagner');
+//localStorage.setItem('sobrenome', 'Pedroso');
+
+
+//console.log(localStorage)
+
+onload = () => {
+    let nome = localStorage.getItem('nome')
+    let h1
+}
+
+
+const atualizar = (element) => {
+console.log(element)
+}
+
+let h1 = document.querySelector('#nome');
+h1.innerHTML = valor;
+
+
---
+++ M2S5/local.js
@@ -0,0 +1,18 @@
+//localStorage
+//INFORMAÇÕES
+localStorage.setItem('idade', '26');
+localStorage.setItem('cor', 'verde');
+
+
+
+
+console.log(localStorage.length)
+
+console.log(localStorage.getItem('idade'))
+
+//Armazena anotação
+
+localStorage.nome = 'Wagner';
+localStorage.sobrenome = 'Pedroso'
+
+console.log(localStorage)
